c3f8da4314c589224cae9762f076f0c6e60a410a
unknown <juan.cortes05@usa.edu.co>
2023-10-17 22:10:31 -0500

    getters y setters

--- src/Objetos/Character/Character.java
+++ src/Objetos/Character/Character.java
@@ -21,6 +21,70 @@ public abstract class  Character {
         this.playerX = playerX;
     }
 
+    public int getPlayerWidth() {
+        return playerWidth;
+    }
+
+    public void setPlayerWidth(int playerWidth) {
+        this.playerWidth = playerWidth;
+    }
+
+    public int getPlayerHeight() {
+        return playerHeight;
+    }
+
+    public void setPlayerHeight(int playerHeight) {
+        this.playerHeight = playerHeight;
+    }
+
+    public int getSpeedBoost() {
+        return speedBoost;
+    }
+
+    public void setSpeedBoost(int speedBoost) {
+        this.speedBoost = speedBoost;
+    }
+
+    public int getPlayerSpeedX() {
+        return playerSpeedX;
+    }
+
+    public void setPlayerSpeedX(int playerSpeedX) {
+        this.playerSpeedX = playerSpeedX;
+    }
+
+    public int getJumpBoost() {
+        return jumpBoost;
+    }
+
+    public void setJumpBoost(int jumpBoost) {
+        this.jumpBoost = jumpBoost;
+    }
+
+    public int getPlayerSpeedY() {
+        return playerSpeedY;
+    }
+
+    public void setPlayerSpeedY(int playerSpeedY) {
+        this.playerSpeedY = playerSpeedY;
+    }
+
+    public Color getPlayerColor() {
+        return playerColor;
+    }
+
+    public void setPlayerColor(Color playerColor) {
+        this.playerColor = playerColor;
+    }
+
+    public int getPlayerLives() {
+        return playerLives;
+    }
+
+    public void setPlayerLives(int playerLives) {
+        this.playerLives = playerLives;
+    }
+
     public int getPlayerY() {
         return playerY;
     }
