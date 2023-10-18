60298b168ffa05d5bc2aee636287109bf79c4092
DavidAvila6 <david.avila02sa.edu.co>
2023-10-18 03:54:23 -0500

    Poderes

--- src/Objetos/Character/CharacterFactory.java
+++ src/Objetos/Character/CharacterFactory.java
@@ -4,3 +4,4 @@ public interface CharacterFactory {
     
         Character createPlayerCharacter();
 }
+ 
--- src/Objetos/SpecialObject.java
+++ src/Objetos/SpecialObject.java
@@ -1,70 +1,34 @@
 package Objetos;
 
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 
 import modelo.GameObject;
 
 public class SpecialObject extends GameObject {
-    private int x;
-    private int y;
-    private int width;
-    private int height;
-    private Color color;
+    private String imagePath; // Nueva propiedad para la ruta de la imagen
 
-    public SpecialObject(int x, int y, int width, int height, Color color) {
-        super(x, y, width, height, color);
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.color = color;
+    public SpecialObject(int x, int y, int width, int height, String imagePath) {
+        super(x, y, width, height);
+        this.imagePath = imagePath;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
     }
 
     public void draw(Graphics g) {
-        g.setColor(color);
-        g.fillRect(x, y, width, height);
-    }
+        // Carga la imagen desde la ruta
+        ImageIcon imageIcon = new ImageIcon(imagePath);
+        Image image = imageIcon.getImage();
 
-    // Métodos Getters y Setters
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
-        this.color = color;
+        // Dibuja la imagen del objeto especial
+        g.drawImage(image, getX(), getY(), getWidth(), getHeight(), null);
     }
 }
